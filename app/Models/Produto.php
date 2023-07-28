0923fa683b681118cdabbf97a0d078ea732899ba
renancreuso <renan_almeida@ufms.br>
2023-07-28 09:53:59 -0300

    Finalizando o projeto/CRUD com front

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -1,12 +1,24 @@
 <?php
 
+
 namespace App\Models;
 
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 use Illuminate\Database\Eloquent\Model;
 
+
 class Produto extends Model
+
 {
+
     use HasFactory;
-    protected $fillable = ['descricao', 'qts', 'precoUnitario', 'precoVendo'];
+
+    protected $fillable = [
+
+        'descricao', 'qtd', 'precoUnitario', 'precoVenda'
+
+    ];
+
 }
